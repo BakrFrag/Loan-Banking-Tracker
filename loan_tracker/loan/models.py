cea7f2b3039f79071cec44e5faa65fffb542d61b
BK <bakr.frag@gmail.com>
2020-01-17 17:52:11 +0200

    create offer model as relation between investor and loan

--- loan_tracker/loan/models.py
+++ loan_tracker/loan/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 
-# Create your models here.
-class Brrower(models.Model):
-    name=models.CharField(max_length=256);
-class Investor(models.Model):
-    name=models.CharField(max_length=256);
-    balance=models.IntegerField();
 LOAN_CHOICES=(
+
     ("funded","Fundded"), # investor submit offer;
     ("complete","Completed") # brrower return mony back to investor
 );
+# Create your models here.
+class Brrower(models.Model):
+    name=models.CharField(max_length=256);
+    def __str__(self):
+        return self.name;
+class Investor(models.Model):
+    name=models.CharField(max_length=256);
+    balance=models.IntegerField();
+    def __str__(self):
+        return self.name;
 class Loan(models.Model):
     duration=models.CharField(default="6 Months",max_length=256);
     amount=models.IntegerField(default=5000.0);
     currency=models.CharField(default="$ Americian Dolar",max_length=256);
     status=models.CharField(choices=LOAN_CHOICES,max_length=256,null=True);
     brrower=models.OneToOneField(Brrower,on_delete=models.CASCADE);
-    investor=models.OneToOneField(Investor,on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Loan With Brrower {self.brrower.name} And Investor {self.investor.name}";
+class Offer(models.Model):
+    loan=models.OneToOneField(Loan,on_delete=models.CASCADE);
+    investor=models.OneToOneField(Investor,on_delete=models.CASCADE);
+    def __str__(self):
+        return f"offer on {self.loan}";
--- loan_tracker/loan/serializers.py
+++ loan_tracker/loan/serializers.py
@@ -5,13 +5,15 @@ from rest_framework import serializers;
 class LoanListSerializer(serializers.ModelSerializer):
     class Meta:
         model=Loan;
-        fields="__all__";
-        read_only_fields=("duration","amount","currency");
+        fields='__all__';
+        
 # loan create serializer Post Request
 class LoanCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model=Loan;
-        exclude=('status',);
+        fields="__all__";
+        read_only_fields=("duration","amount","currency","status")   
+       
 # brrower serializer
 class BrrowerSerializer(serializers.ModelSerializer):
     class Meta:
