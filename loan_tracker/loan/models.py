77766e70b565737ab341a9725b60e7826e790276
BK <bakr.frag@gmail.com>
2020-01-17 10:30:34 +0200

    just create init models for brrower , investor and loan changes like validators and triggers will applay

--- loan_tracker/loan/models.py
+++ loan_tracker/loan/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Brrower(models.Model):
+    name=models.CharField(max_length="256");
+class Investor(models.Model):
+    name=models.CharField](max_length="256");
+    balance=models.IntegerField();
+class Loan(models.Model):
+    duration=models.CharField(default="6 Months",max_length="256");
+    amount=models.IntegerField(default=5000.0);
+    currency=models.CharField(default="$ Americian Dolar",max_length="256");
+    brrower=models.OneToOneField(Brrower,on_delete=Models.CASCADE);
+    investor=models.OneToOneField(Investor,on_delete=models.CASCADE)
