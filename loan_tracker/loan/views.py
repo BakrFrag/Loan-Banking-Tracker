55fe65e192eabfcbd66ec100b9cab8904c6aa0a9
BK <bakr.frag@gmail.com>
2020-01-17 11:23:30 +0200

    just create views for list and create views more logic will be added

---
+++ loan_tracker/loan/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import generics;
+# Create your views here.
+fron loan.serializers import (LoanListSerializer,LoanCreateSerializer,BrrowerSerializer,InvestorSerializer);
+from loan.models import (Brrower,Loan,Investor);
+class LoanListView(generics.ListAPIView):
+      model=Loan;
+      queryset=Loan.objects.all();
+      serializer_class=LoanListSerializer;
+class LoanCreateView(generics.CreateAPIView):
+    model=Loan;
+    serializer_class=LoanCreateSerializer;
+class BrowerListView(generics.ListAPIView):
+    model=Brrower;
+    serializer_class=BrowerSerializer;
+    queryset=Brrower.objects.all();
+class BrrowerCreateView(generics.CreateAPIView):
+    model=Brrower;
+    serializer_class=BrowerSerializer;
+class InvestorListView(generics.ListAPIView):
+    model=Investor;
+    queryset=Investor.objects.all();
+    serializer_class=InvestorSerializer;
+class InvestorCreateView(generics.CreateAPIView):
+    model=Investor;
+    serializer_class=InvestorSerializer;
+
