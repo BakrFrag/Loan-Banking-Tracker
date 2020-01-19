15517944e146cac975eaa52b77c0252ef72ec7c9
BK <bakr.frag@gmail.com>
2020-01-19 19:36:21 +0200

    add test cases to list all loans and create loans

--- loan_tracker/loan/tests.py
+++ loan_tracker/loan/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient;
+from django.urls import reverse;
 # Create your tests here.
+"""
+test case to list loans GET REQUEST with status code 200
+"""
+class TestListLoan(TestCase):
+    def test_loanlist(self):
+        client=APIClient();
+        response=client.get(reverse("loan_list"),format='json');
+        self.assertEqual(response.status_code,200);
+"""
+test case to test create loan with status code 400
+"""
+class TestCreateLoan(TestCase):
+    def test_createloan(self):
+        client=APIClient();
+        response=client.post(reverse("create_loan",kwargs={name:"test_brrower"}),format="json");
+         self.assertEqual(response.status_code,400);
+
