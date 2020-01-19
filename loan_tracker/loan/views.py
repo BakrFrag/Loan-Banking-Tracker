cc0b238cd5940561544ece086d43e7eafa8b7f0c
BK <bakr.frag@gmail.com>
2020-01-19 09:10:44 +0200

    add filters on views with get request also add multi line comment before each view

--- loan_tracker/loan/views.py
+++ loan_tracker/loan/views.py
@@ -1,34 +1,104 @@
 from django.shortcuts import render
 from rest_framework import generics;
+from rest_framework.exceptions import APIException;
 # Create your views here.
-from loan.serializers import (LoanListSerializer,LoanCreateSerializer,BrrowerSerializer,InvestorSerializer,OfferSerializer);
+from loan.serializers import (LoanListSerializer,
+LoanCreateSerializer,BrrowerSerializer,InvestorSerializer,OfferSerializer);
 from loan.models import (Brrower,Loan,Investor,Offer);
+
+"""
+List All Loans Or Custimize Loan By Status Either 
+funded or completed
+GET REQUEST
+"""
 class LoanListView(generics.ListAPIView):
       model=Loan;
-      queryset=Loan.objects.all();
       serializer_class=LoanListSerializer;
+      def get_queryset(self):
+            status=self.request.GET.get('status',None);
+            if status is not None:
+                return Loan.objects.filter(status=status);
+            return Loan.objects.all();
+
+"""
+create loan instance of loan model 
+POST REQUEST
+"""
+
 class LoanCreateView(generics.CreateAPIView):
     model=Loan;
     serializer_class=LoanCreateSerializer;
+
+"""
+List All Brrowers Or Custimize Brrower List 
+By Thier Names
+GET REQUEST
+"""
 class BrowerListView(generics.ListAPIView):
     model=Brrower;
     serializer_class=BrrowerSerializer;
-    queryset=Brrower.objects.all();
+    def get_queryset(self):
+        name=self.request.GET.get('name',None)
+        if name is not None:
+            return Brrower.objects.filter(name=name);
+        return Brrower.objects.all();
+"""
+Create Brrower 
+POST REQUEST
+"""
 class BrrowerCreateView(generics.CreateAPIView):
     model=Brrower;
     serializer_class=BrrowerSerializer;
+
+"""
+List All Investors Or Just Custimize Them By Name
+GET REQUEST
+"""    
 class InvestorListView(generics.ListAPIView):
     model=Investor;
     queryset=Investor.objects.all();
     serializer_class=InvestorSerializer;
+    def get_queryset(self):
+        name=self.request.GET.get('name',None)
+        if name is not None:
+            return Investor.objects.filter(name=name);
+        return Investor.objects.all();
+        
+"""
+Create Investor
+POSTREQUEST
+"""
 class InvestorCreateView(generics.CreateAPIView):
     model=Investor;
     serializer_class=InvestorSerializer;
-
+"""
+List All Offers
+GET REQUEST
+"""
 class OfferListView(generics.ListAPIView):
     model=Offer;
     serializer_class=OfferSerializer;
     queryset=Offer.objects.all();
+
+"""
+Create Offer
+Validation Add By First Check
+Money Mount = (Loan Amount + Fee ) = 5000 + 3 = 5003 
+If Investor.Balance >= 5003
+Will Create Loan
+Else APIException Will Riased
+POST REQUEST
+"""
 class OfferCreateView(generics.CreateAPIView):
     model=Offer;
     serializer_class=OfferSerializer;
+    def perform_create(self,serializer):
+        if self.investor.balance >= 5003:
+            balance -= 5003;
+            status="funded";
+            total=5750;
+            return serializer.save(
+                self.loan.status=status,
+                self.loan.total=total,
+                self.investor.balance=balance);
+        raise APIException("Investor Don't Have enough Balance Money")
