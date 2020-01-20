61961c55dd8be7d1b6964b994367a2d7613ba76f
BK <bakr.frag@gmail.com>
2020-01-20 09:14:34 +0200

    add coreapi documentation also

--- loan_tracker/loan_tracker/urls.py
+++ loan_tracker/loan_tracker/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path,include;
+
 # swagger view
 from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Loan Tracker')
+# coreapi documentation
+from rest_framework.documentation import include_docs_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('loan.urls')),
-    path("swagger-docs",schema_view)
+    path("swagger-docs/",schema_view),
+    path("core-docs/",include_docs_urls(title="Loan Tracker"))
 ]
