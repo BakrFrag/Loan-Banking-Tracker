83ae4f2e068925a47bce5d7b84083ab2b937dfdc
BK <bakr.frag@gmail.com>
2020-01-20 07:27:50 +0200

    add nested serializer to loan and offer to show details of investors and brrowers

--- loan_tracker/loan/serializers.py
+++ loan_tracker/loan/serializers.py
@@ -1,11 +1,8 @@
 from loan.models import Brrower, Investor , Loan,Offer ;
 from rest_framework import serializers;
 
-# loan list/retrieve serializer Get Request
-class LoanListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Loan;
-        fields='__all__';
+
+
         
 # loan create serializer Post Request
 class LoanCreateSerializer(serializers.ModelSerializer):
@@ -24,11 +21,21 @@ class InvestorSerializer(serializers.ModelSerializer):
     class Meta:
         model=Investor;
         fields="__all__";
+
+# loan list/retrieve serializer Get Request
+class LoanListSerializer(serializers.ModelSerializer):
+    brrower=BrrowerSerializer()
+    class Meta:
+        model=Loan;
+        fields='__all__';
 """
 add validation on offer serializer to ensure that 
 investor balance have more than 5003 (loan amount + fee)
 """
+
 class OfferSerializer(serializers.ModelSerializer):
+    loan=LoanListSerializer();
+    investor=InvestorSerializer();
     class Meta:
         model=Offer;
         fields="__all__";
